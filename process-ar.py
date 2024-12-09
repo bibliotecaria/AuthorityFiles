d5069f4808729744b507013a99ba279d3762f6f8
Melanie Polutta <bibliotecaria.mel@gmail.com>
2024-12-09 15:57:11 -0500

    recording thought process of program

--- process-ar.py
+++ process-ar.py
@@ -1,8 +1,22 @@
+'''This program will search the authority file based on the identifier entered 
+and then search up and down the hierarchy of the term.
+To run the search, extract the id.loc.gov URI for the term you want to search, and plug that into the program that will be created.
+	1. JSON file is organized as an array of dicts. We are looking for the specific one in which the last part of the character of the @id key matches the LCCN that we have plugged in.
+		a. Make sure to remove space from LCCN.
+	2. Get a heading ID (the LCCN transformed for the URI) and fetch the JSON based on that ID
+	3. Identify the hash (or dict) in the JSON array that contains the information desired.
+		a. Go through list of dicts until we find the one that matches the @ID key matches the heading
+         ID we've entered.
+	4. Establish separate functions for extracting the information from the 010, 1XX, 4XX, 5XX, NTs, and 680.
+Need to maintain a list of already-processed headings so as not to repeat recursion.
+'''
+
 import sys
 import requests
 import json
 import argparse
 import csv
+
 
 
 PREFIX="://id.loc.gov/authorities/"
@@ -12,6 +26,12 @@ TYPE={"mp":"performanceMediums/",
       "dg":"demographicTerms/", 
       "gf":"genreForms/"}
 recordset = {}
+
+def matches_key(key, element):
+    if key in element:
+        return True
+    else:
+        return False
 
 def get_authlabel(sub):
     #http://www.loc.gov/mads/rdf/v1#authoritativeLabel
@@ -36,14 +56,32 @@ def get_notes(sub):
         retval = note["@value"] + " | "
     return(retval)
 
-def get_references(sub):
+def get_references(record):
     #http://www.loc.gov/mads/rdf/v1#variantLabel
-    seeref = sub["http://www.loc.gov/mads/rdf/v1#variantLabel"]
+    #http://www.loc.gov/mads/rdf/v1#Temporal
+    #http://www.loc.gov/mads/rdf/v1#ComplexSubject
+    #http://www.loc.gov/mads/rdf/v1#Topic
+    key = "http://www.loc.gov/mads/rdf/v1#variantLabel"
+    variantlist = []
+    reflist = [element for element in record if matches_key(key, element)]
+    for ref in reflist:
+        if "http://www.loc.gov/mads/rdf/v1#Temporal" not in ref["@type"]:
+            var = ref[key][0]
+            label = var["@value"]
+            if label not in variantlist:
+                variantlist.append(label)
     return(None)
 
 
 def process_record(sub):
     authlabel = get_authlabel(sub)
+
+def process_uri(uri):
+    # construct URN from URI
+    # get record from API
+    # create sub
+    # call process_record, which will return a hash
+    # put hash on hash stack by URI as key
 
 if __name__ == "__main__":
     # command line arguments
@@ -63,7 +101,15 @@ if __name__ == "__main__":
     req = requests.get(url)
     #requests will follow redirects
     record = json.loads(req.text)
+    get_references(record)
+    matches = []
     for sub in record:
         if sub["@id"] == baseuri:
-            print("yeah")
+            matches.append(sub)
+    #make sure that at least one match, report out of there is more than one match
+    if len(matches) == 0:
+        sys.exit()
+    elif len(matches) > 1:
+        print(f"{baseurl} has more than one ID match")
+    process_record(matches[0])
     csvfile = args.o
