42a136c5a252993b0f7c48dfd6d1f394d58f1ece
Melanie Polutta <bibliotecaria.mel@gmail.com>
2025-01-24 17:02:13 -0500

    Adding substantial improvements

--- process-ar.py
+++ process-ar.py
@@ -25,7 +25,18 @@ TYPE={"mp":"performanceMediums/",
       "sj":"childrensSubjects/", 
       "dg":"demographicTerms/", 
       "gf":"genreForms/"}
+# recordset contains a list of hashes with two keys: URI, processed(Boolean)
+recordlist = []
 recordset = {}
+# TODO how to handle constantly changing list of hashes
+idlist = []
+# here is the list of LCCNs
+
+def add_to_list(id):
+    add = True
+    if id in idlist or id in recordlist.keys():
+        add = False
+    return(add)
 
 def matches_key(key, element):
     if key in element:
@@ -73,11 +84,31 @@ def get_references(record):
                 variantlist.append(label)
     return(" | ".join(variantlist))
 
+def get_rel_ids(sub, has):
+    # here is where we extract BTids, NTids, RTids
+    #http://www.loc.gov/mads/rdf/v1#hasBroaderAuthority
+    #http://www.loc.gov/mads/rdf/v1#hasNarrowerAuthority
+    #http://www.loc.gov/mads/rdf/v1#hasReciprocalAuthority
+    namespace = "http://www.loc.gov/mads/rdf/v1#has" + has
+    auths = sub[namespace]
+    ids = []
+    for auth in auths:
+        uri = auth["@id"]
+        if uri is not None:
+            id = uri.split("/")[-1]
+            ids.append(id)
+            if add_to_list(id):
+                idlist.append(id)
+    return(ids)
 
-def process_record(sub):
+def process_sub(sub):
+    # here is where we extract data from subs of the record
     authlabel = get_authlabel(sub)
-    variants = get_references(sub)
-
+    notes = get_notes(sub)
+    broads = get_rel_ids(sub, "BroaderAuthority")
+    narrows = get_rel_ids(sub, "NarrowerAuthority")
+    reciprocals = get_rel_ids(sub, "ReciprocalAuthority")
+    return(authlabel, notes, broads, narrows, reciprocals)
 
 def requesting(url):
     record = None
@@ -85,7 +116,7 @@ def requesting(url):
     if req.status_code == 200:
     # requests will follow redirects
         record = json.loads(req.text)
-    # TODO: log status errors
+    # TODO log status errors
     return(record)
 
 def process_uri(uri):
@@ -98,6 +129,62 @@ def process_uri(uri):
     # put hash on hash stack by URI as key
     return(None)
 
+def handle_id(id):
+    # needs to return status because sys.exit won't work here -- need TRUE/FALSE for testing
+    # take id and make full madsrdf.json record from which to extract data via API
+    id = id.replace(" ","")
+    if id[0:2] not in TYPE:
+        print(f"Type {id[0:2]} not supported ({id}).")
+        return(None, None)
+    else:
+        baseurl = "https" + PREFIX + TYPE[id[0:2]] + id
+        baseuri = "http" + PREFIX + TYPE[id[0:2]] + id
+        url = baseurl + ".madsrdf.json"
+    print(url)
+    record = requesting(url)
+    if record is None:
+        print("unable to find record")
+        return(None, None)
+    variants = get_references(record)
+    sub_groups = []
+    for sub in record:
+        if sub["@id"] == baseuri:
+            sub_groups.append(sub)
+    #make sure that at least one match, report out of there is more than one match
+    if len(sub_groups) == 0:
+        print(f"No sub groups in {id}.")
+    elif len(sub_groups) > 1:
+        print(f"{id} has more than one ID sub group")
+    return(variants, sub_groups)
+    # needs to move process_record(sub_groups[0])
+
+def process_id(id):
+    # here is where we create URI and URL (in subroutine), get data, also subprocessing for data
+    variants, subs = handle_id(id)
+    if variants is None and subs is None:
+        print("Cannot find information")
+        return()
+    # TODO define what is returned
+    # recordset needs LCCN, authlabel, variants, notes, BTid, NTid, RTid
+    authlabel, notes, broads, narrows, reciprocals = process_sub(subs[0])
+    # recordset["sh3443"] = {'auth'='foo', 'variant'=[alist],'notes'=[listof notes], 'bIds='sh233|sh99'}
+    recordset[id] = {
+        "authlabel":authlabel, 
+        "notes":notes, 
+        "broads":broads, 
+        "narrows":narrows, 
+        "reciprocals":reciprocals,
+        "variants":variants}
+
+def refine_recordset():
+    # TODO find authlabel for BTid, NTid, RTid
+    pass
+
+def process_list():
+    for id in idlist:
+        process_id(id)
+
+
 if __name__ == "__main__":
     # command line arguments
     parser = argparse.ArgumentParser(description='Get options.')
@@ -106,25 +193,16 @@ if __name__ == "__main__":
     args = parser.parse_args()
     #print("we parsed")
     id = args.id.replace(" ","")
-    if id[0:2] not in TYPE:
-        sys.exit(f"Type {id[0:2]} not supported.")
-    else:
-        baseurl = "https" + PREFIX + TYPE[id[0:2]] + id
-        baseuri = "http" + PREFIX + TYPE[id[0:2]] + id
-        url = baseuri + ".madsrdf.json"
-    print(url)
-    record = requesting(url)
-    if record is None:
-        print("unable to find record")
-        sys.exit()
-    matches = []
-    for sub in record:
-        if sub["@id"] == baseuri:
-            matches.append(sub)
-    #make sure that at least one match, report out of there is more than one match
-    if len(matches) == 0:
-        sys.exit()
-    elif len(matches) > 1:
-        print(f"{baseurl} has more than one ID match")
-    process_record(matches[0])
+    idlist.append(id)
+    process_list()
+    refine_recordset()
+    # TODO write out recordset
     csvfile = args.o
+
+
+
+
+"""     variants, sub_groups = handle_id(id)
+    # TODO handle errors
+    process_record(sub_groups[0])
+ """
